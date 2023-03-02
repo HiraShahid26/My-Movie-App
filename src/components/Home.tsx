be8da5d95ad78e5e823d85e48cb14c630541f959
Hira Shahid <hira_shahid_26@hotmail.com>
2023-03-02 17:18:07 +0100

    api call for categories and movies

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import {MovieType, CategoryType} from '../models/models'
+import {getAll} from '../api/movies'
+import { getAllCategories } from '../api/category'
+
 
 export const Home = () => {
+    const [movies, setMovies] = useState<MovieType[]>([])
+    const [categories, setCategories] = useState<CategoryType[]>([])
+    console.log(movies)
+    const getMovies = async () => {
+        const data = await getAll() 
+        setMovies(data)
+    }  
+    const getCategories = async () => {
+        const genre = await getAllCategories() 
+        setCategories(genre)
+    }  
+
+    useEffect(() => {
+        getMovies()
+        getCategories()
+    }, [])
   return (
     <div>Home</div>
   )
+
 }
--- src/models/models.tsx
+++ src/models/models.tsx
@@ -1,1 +1,20 @@
-export {}
+export interface MovieType {
+    id: number;
+    backdrop_path: string;
+    poster_path: string;
+    title: string;
+    release_date: string;
+}
+
+export interface CategoryType {
+    id: number
+    name: string
+}
+
+export interface DetailsType {
+    language: string;
+    title: string;
+    popularity: number;
+    release_date: string
+    backdrop_path: string;
+}
