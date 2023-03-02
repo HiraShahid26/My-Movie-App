7a05f9aea1b630aa40b4fb0eee01d459d43870c0
Hira Shahid <hira_shahid_26@hotmail.com>
2023-03-02 15:46:33 +0100

    feat: adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home' ;
+import { MovieDetails } from './pages/MovieDetails';
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />}/> 
+          <Route path="/movie-details/:id" element={<MovieDetails />}/> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
