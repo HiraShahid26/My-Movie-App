beb2615d04376013504863488c9e5d44cad8c5d0
nab-sa <nab_s_a@protonmail.com>
2023-03-09 10:57:18 +0100

    fixing overflow and cleaning code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { getAllCategories } from "../api/category";
 import { MoviesList } from "../components/MoviesList";
 import { CategoryList } from "../components/CategoryList";
-import { useMovieContext } from "../context/MovieContext";
-import { CategoryType } from "../models/categoryModel";
 
 export const Home = () => {
   return (
-    <div>
-      <div className="flex w-screen">
-        <CategoryList />
-        <MoviesList />
-      </div>
+    <div className="flex w-full">
+      <CategoryList />
+      <MoviesList />
     </div>
   );
 };
